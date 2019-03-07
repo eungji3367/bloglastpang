ea4da0495be89ac21438fd2a6e45ae096a8dd084
eunji <lewq123@likelion.org>
2019-03-07 20:49:53 +0900

    initial commit

---
+++ likelion_blog/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\김다혜\desktop\멋사\project\likelion_blog\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
